f54e57472bfcc87ff881cbd789407796c25466b4
unknown <vuhuyquang2k@gmail.com>
2024-07-15 02:59:11 +0700

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,6 +41,47 @@ export default function Banner() {
             </p>
           </div>
         </div>
-      </div></>
+      </div>
+      <div className="h-screen flex items-center justify-center">
+        <div className="h-3/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 container-service text-[16px]">
+          <div className="service-item px-5 py-20 bg-white text-dark flex flex-col gap-5">
+            <div>
+              <Image height={80} width={80} src="/icons/svg/web-development.svg" alt="Phát triển Web" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <h3 className="heading mb-3 service-name">Phát triển Web</h3>
+              <p>Xây dựng và phát triển các ứng dụng web.</p>
+            </div>
+          </div>
+          <div className="service-item px-5 py-20 bg-[#1e1e1e] text-white flex flex-col gap-5">
+            <div>
+              <Image height={80} width={80} src="/icons/svg/web-design.svg" alt="Viết Tool" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <h3 className="heading mb-3 service-name">Viết Tool</h3>
+              <p>Tạo ra các công cụ tự động, tối ưu hóa quy trình làm việc.</p>
+            </div>
+          </div>
+          <div className="service-item px-5 py-20 bg-white text-dark flex flex-col gap-5">
+            <div>
+              <Image height={80} width={80} src="/icons/svg/server-proxy-svgrepo-com.svg" alt="Proxy Server" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <h3 className="heading mb-3 service-name">Proxy Server</h3>
+              <p>Giúp bạn tạo ra các proxy server nhanh chóng và dễ dàng.</p>
+            </div>
+          </div>
+          <div className="service-item px-5 py-20 bg-[#fdcb6e] text-dark flex flex-col gap-5">
+            <div>
+              <Image height={80} width={80} src="/icons/svg/writing.svg" alt="Viết Blog" />
+            </div>
+            <div className="flex flex-col gap-5">
+              <h3 className="heading mb-3 service-name">Viết Blog</h3>
+              <p>Chia sẻ kiến thức, tạo kết nối với độc giả và tác giả thông qua các bài viết.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
