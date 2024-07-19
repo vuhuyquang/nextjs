c90a6db0c15afbe7c45f366efd38318b688e5eb0
quangvh18 <quangvh18@ghtk.co>
2024-07-19 18:29:05 +0700

    update

--- src/app/bai-viet/[slug]/page.tsx
+++ src/app/bai-viet/[slug]/page.tsx
@@ -41,14 +41,14 @@ export default function PostDetail({ params }: { params: { slug: string } }) {
   const sanitizedContent = DOMPurify.sanitize(post.fullContent);
 
   return (
-    <div className="max-w-3xl mx-auto p-5">
+    <div className="max-w-3xl mx-auto p-5 overflow-hidden">
       <h1 className="mb-5 text-3xl md:text-4xl font-bold tracking-tight text-gray-800 hover:text-gray-900 transition duration-300 cursor-pointer">
         {post.title}
       </h1>
       <p className="mb-5 text-lg md:text-xl text-gray-600">{post.shortContent}</p>
-      <img className="w-full mb-5 rounded-lg" src={post.imageUrl} alt={post.title} />
-      <div className="mb-5 flex items-center justify-between">
-        <div className="flex justify-between gap-2 items-center xl:flex">
+      <img className="w-full mb-5 rounded-lg object-cover" src={post.imageUrl} alt={post.title} />
+      <div className="mb-5 flex items-center justify-between flex-wrap">
+        <div className="flex items-center gap-2">
           <Image
             height={26}
             width={26}
@@ -58,13 +58,15 @@ export default function PostDetail({ params }: { params: { slug: string } }) {
           />
           <span className="text-[#292929] text-[10px] lg:text-sm xl:text-sm">{post.author}</span>
         </div>
-        <div className="flex justify-between gap-5 text-[#979797] items-center text-[10px] lg:text-sm xl:text-sm">
-          <span className="rounded-full bg-[#f2f2f2] px-2 py-1 flex justify-between items-center">{post.category}</span>
+        <div className="flex items-center gap-5 text-[#979797] text-[10px] lg:text-sm xl:text-sm flex-wrap">
+          <span className="rounded-full bg-[#f2f2f2] px-2 py-1 flex items-center">{post.category}</span>
           <span>{post.createdAt}</span>
           <span>{post.readingTime}</span>
         </div>
       </div>
-      <div className="text-gray-700 mb-5 text-lg leading-relaxed" style={{ lineHeight: '1.75' }} dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
+      <div className="text-gray-700 mb-5 text-lg leading-relaxed">
+        <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
+      </div>
     </div>
   );
 };
