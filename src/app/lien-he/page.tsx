300d866d1a4504b6c58173cb475ad19f8cec87a3
quangvh18 <quangvh18@ghtk.co>
2024-08-12 17:29:13 +0700

    update

--- src/app/lien-he/page.tsx
+++ src/app/lien-he/page.tsx
@@ -1,5 +1,20 @@
 export default function Contact() {
   return (
-    <div>Sắp ra mắt</div>
+    <div id="contact-custom" className="w-screen min-h-screen">
+      <div className="flex justify-center">
+        <div className="w-screen h-[450px] md:h-[600px] py-10">
+          <iframe
+            className="w-full"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.5713876763016!2d105.78160337587204!3d21.009811488427854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acab11ec72b9%3A0x4a34e18cc7b3b035!2zUC4gxJDhu5cgxJDhu6ljIEThu6VjLCBN4buFIFRyw6wsIE5hbSBU4burIExpw6ptLCBIw6AgTuG7mWksIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1723001579181!5m2!1sen!2s"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    </div>
   );
 }
